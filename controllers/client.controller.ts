1b610222836c3627553b695a3c282111ec14b166
tylor-canton <tylor.canton@conduent.com>
2021-06-05 21:13:56 -0400

    Controller updates

--- controllers/client.controller.ts
+++ controllers/client.controller.ts
@@ -2,13 +2,28 @@ import express from 'express';
 import { ato, ReE, ReS } from '../utilities/jsend';
 
 const ClientController = {
-
     vOneparse: async (req: express.Request, res: express.Response) => {
-        res.setHeader('Content-Type', 'application/json');
-        
+
         try {
             const data = req.body.data;
-            const propArrVOne = data.split("000");
+            const propArrTwo = data.split(/[.0+\d](0+)/g);
+            const [firstnameA, firstnameB, lastnameA, lastnameB, clientId] = propArrTwo;
+
+            return ReS(res, {
+                firstname: `${firstnameA}${firstnameB}`,
+                lastname: `${lastnameA}${lastnameB}`,
+                clientId: `${clientId}`
+            }, 200);
+
+        } catch (error) {
+            ReE(res, error, 400);
+        }
+    },
+    vTwoparse: async (req: express.Request, res: express.Response) => {
+
+        try {
+            const data = req.body.data;
+            const propArrVOne = data.match(/[^0]+(?:00[^0]+)*/g);
             const [firstname, lastname, clientId] = propArrVOne;
             return ReS(res, {
                 firstname, lastname, clientId
@@ -16,20 +31,7 @@ const ClientController = {
         } catch (error) {
             ReE(res, error, 400);
         }
-
-
-
     },
-    vTwoparse: async (req: express.Request, res: express.Response) => {
-        res.setHeader('Content-Type', 'application/json');
 
-        const data = req.body.data;
-        const propArrVOne = data.replace(/[^a-z0-9]/gi, '').split("000").map((e: any) => e.replace(/0/g, ''));
-        const [firstname, lastname, clientId] = propArrVOne;
-
-        return ReS(res, {
-            firstname, lastname, clientId
-        }, 200);
-    }
 }
 export default ClientController;
